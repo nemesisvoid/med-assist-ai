1cdd8fd6b52780b8ad4cf5d60a846683c6b95232
nemesisvoid <samsonajibade40@gmail.com>
2026-05-16 19:04:55 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+    tsconfigPath: 'tsconfig.json',
+  },
+  turbopack: {
+    resolveAlias: {
+      canvas: './empty-module.js',
+    },
+  },
 };
 
 export default nextConfig;
